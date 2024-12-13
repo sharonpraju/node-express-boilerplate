c79165d5540fd1cb63ff1e58f642b952f1ddb1a0
sharonpraju <sharonpraju@gmail.com>
2024-12-14 03:07:07 +0530

    improvements on response handler

--- app/utils/response-handler.js
+++ app/utils/response-handler.js
@@ -20,7 +20,7 @@ exports.error_function = function (api_data) {
         "success": false,
         "statusCode": api_data.status,
         "data": api_data.data ? api_data.data : null,
-        "message": api_data.message ? api_data.message : null
+        "message": api_data.message ? (typeof api_data.message == "string" ? api_data.message : JSON.stringify(api_data.message)) : null
     };
     return response;
 }
